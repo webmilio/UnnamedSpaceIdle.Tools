04e76978e19054d052e23d14710617fec71c1a86
webmilio <webmilio@users.noreply.github.com>
2025-10-24 21:12:14 +0200

    Deleted misnamed project

--- Base.LayoutCreator.Common/Tile.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-using System.Collections.Generic;
-
-namespace Base.LayoutCreator.Common;
-
-public abstract class Tile
-{
-    public abstract Dictionary<string, double[,]> GetMultiplier(Tile[,] grid, int x, int y);
-}
